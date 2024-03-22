c69ca0139b723eb41bb22b719444873ebf8c1130
Kami <kawarento@gmail.com>
2024-03-22 20:10:58 +0300

    Нет такого понятия, как «судьба». Это всего лишь сочетание нескольких обстоятельств.

---
+++ Practice 5/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите значение x: ");
+                double x = double.Parse(Console.ReadLine());
+                double y = (2 * x / Math.Sin(x)) - 4 *x ;
+                Console.WriteLine($"Значение выражения: {y}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("деления на ноль");
+            }
+
+            Console.WriteLine();
+
+            try
+            {
+                Console.Write("Введите значение x: ");
+                double x = double.Parse(Console.ReadLine());
+                double y = 1 / Math.Pow(Math.Tan(x - 1), 2);
+                Console.WriteLine($"Значение выражения: {y}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("деления на ноль");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Practice 5/Task3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double A, B, C, D;
+
+                Console.Write("Введите значение A: ");
+                A = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите значение B: ");
+                B = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите значение C: ");
+                C = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите значение D: ");
+                D = double.Parse(Console.ReadLine());
+
+                Swap(ref A, ref B);
+
+                Swap(ref C, ref D);
+
+                Swap(ref B, ref C);
+
+                Console.WriteLine("A: " + A);
+                Console.WriteLine("B: " + B);
+                Console.WriteLine("C: " + C);
+                Console.WriteLine("D: " + D);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("деления на ноль");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void Swap(ref double x, ref double y)
+        {
+            double temp = x;
+            x = y;
+            y = temp;
+        }
+
+    }
+}
