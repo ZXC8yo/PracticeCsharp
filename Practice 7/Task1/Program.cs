6de74c86a2b1ad49ca77c6911a86a679e6f1d4f0
Kami <kawarento@gmail.com>
2024-03-22 20:13:32 +0300

    Некое незримое существо или закон, подобно Длани Господней парящей над миром?

---
+++ Practice 7/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст ");
+            string s = Console.ReadLine();
+            Console.Write("Сколько букв в слове ");
+            double n = double.Parse(Console.ReadLine());
+
+            string[] mas = s.Split(' ');
+
+            foreach (var S in mas) 
+            {
+                if (n >= S.Length)
+                {
+                    Console.WriteLine(S);
+                }               
+                
+            }
+        }
+    }
+}
---
+++ Practice 7/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string s = Console.ReadLine();
+
+            string[] word = s.Split(' ');
+            string max = word[0];
+
+            foreach (var T in word)
+                {
+                    if (max.Length < T.Length)
+                    {
+                        max = T;
+                    }
+                }
+
+            Console.WriteLine($"{max}, {max.Length}");
+        }
+    }
+}
