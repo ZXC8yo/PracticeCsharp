3bf6845317126097b967044899f3caeb116add0b
Kami <kawarento@gmail.com>
2024-03-24 14:10:05 +0300

    Успокойся, всё в порядке. Я всегда рядом. Успокойся, всё в порядке. Я всегда рядом. Я всегда рядом. У-успокойся, всё в порядке.

---
+++ Practice 18/WeatherLibrary/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WeatherLibrary;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherForecast weatherForecast = new WeatherForecast();
+            string location = "Санкт-Петербурге";
+            string weather = weatherForecast.GetRandomWeather(location);
+            Console.WriteLine(weather);
+        }
+    }
+}
---
+++ Practice 18/WeatherLibrary/WeatherLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WeatherLibrary
+{
+    public class WeatherForecast
+    {
+        private static  string[] WeatherConditions = { "Солнечно", "Облачно", "Дождливо", "Гроза" };
+
+        public string GetRandomWeather(string location)
+        {
+            Random random = new Random();
+            string randomWeatherCondition = WeatherConditions[random.Next(WeatherConditions.Length)];
+            return $"Погода в {location} — {randomWeatherCondition}.";
+        }
+    }
+}
