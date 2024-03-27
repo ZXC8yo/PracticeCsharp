7835c373a36eae52f956fe16656f0947e97682fd
Kami <kawarento@gmail.com>
2024-03-27 12:05:36 +0300

    Чем ярче будет сиять его свет, тем мрачнее будут тени за его спиной.

---
+++ Practice 21/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+
+        public static double Solution1(int a)
+        {
+            return (Math.Pow(((1 + a + Math.Pow(a,2)) / 2 * a *Math.Pow(a,2)) + 2 - ((1 - a + Math.Pow(a,2))/2 * a * Math.Pow(a,2)), -1)) * 5 - 2 * Math.Pow(a,2);
+
+        }
+
+        public static double Solution2(int a)
+        {
+            return (4 - Math.Pow(a,2))/2;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a:");
+            int a = int.Parse(Console.ReadLine());
+            
+            Task[] tasks = new Task[2];
+
+
+            tasks[0] = Task.Run(() =>
+            {
+                Console.WriteLine($"Задача 1 запущена c параметром A{a}");
+                double answer = Solution1(a);
+                Thread.Sleep(2000);
+                Console.WriteLine($"Задача 1 завершена c результатом:{answer}");
+            });
+
+
+            tasks[1] = Task.Run(() =>
+            {
+                Console.WriteLine($"Задача 1 запущена c параметрами A:{a}");
+                double answer = Solution2(a);
+                Thread.Sleep(3000);
+                Console.WriteLine($"Задача 1 завершена c результатом:{answer}");
+            });
+
+
+            Task.WaitAll(tasks);
+            Console.WriteLine("Все задачи завершены");
+
+
+            Task.WaitAny(tasks);
+            Console.WriteLine("Хотя бы одна задача завершена");
+        }
+    }
+
+}
---
+++ Practice 21/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0;
+            int B = 10;
+
+
+            Parallel.For(A, B + 1, i =>
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя");
+                    i += 1;
+                }
+                double result = MyFunction(i);
+                Console.WriteLine("Значение функции для {0}: {1}", i, result);
+            });
+        }
+
+        private static double MyFunction(int x)
+        {
+
+            return x * Math.Sin(x);
+        }
+    }
+}
